6078eee143cdd8f61d64d6ecbd42a78041414005
subimoloma <118798801+subimoloma@users.noreply.github.com>
2025-06-28 16:56:06 +0400

    txn history

--- hcl-portfolio-management-system/src/pages/portfolioPage.jsx
+++ hcl-portfolio-management-system/src/pages/portfolioPage.jsx
@@ -11,7 +11,7 @@ import axios from "axios";
 
 function PortfolioPage() {
   const [historyData, setHistoryData] = useState([]);
-  const [tabValue, setTabValue] = useState(0);
+  const [tabValue, setTabValue] = useState("Orders");
   const [orderId, setOrderId] = useState("");
   const [securityName, setSecurityName] = useState("");
 
@@ -32,15 +32,6 @@ function PortfolioPage() {
       if (query !== "") query += "&";
       query += `asset_id=${securityName}`;
     }
-
-    // if (orderId && orderId.trim() !== "") {
-    //   axios
-    //     .get(`http://localhost:3001/orders?id=${orderId}`)
-    //     .then((res) => {
-    //       setHistoryData(res.data); // json-server returns array
-    //     })
-    //     .catch((err) => console.error("Error fetching order:", err));
-    // }
     if (query !== "") {
       axios
         .get(`http://localhost:3001/orders?${query}`)
@@ -64,8 +55,8 @@ function PortfolioPage() {
         aria-label="Portfolio Tabs"
       >
         <Tab label="Orders" />
-        <Tab label="Security" />
-        <Tab label="Audit Action" />
+        {/* <Tab label="Security" />
+        <Tab label="Audit Action" /> */}
       </Tabs>
 
       {/* Heading */}
@@ -73,21 +64,26 @@ function PortfolioPage() {
         variant="h6"
         sx={{ mt: 2, mb: 2, fontWeight: "bold" }}
       >
-        Portfolio Orders
+         Transaction History
       </Typography>
 
       {/* Search Box */}
-      <Box sx={{ display: "flex", gap: 1, mb: 2 }}>
+      {tabValue == "Orders" && <><Box sx={{ display: "flex", gap: 1, mb: 2 }}>
         <input
           placeholder="Enter Order Id"
           value={orderId}
           onChange={(e) => setOrderId(e.target.value)}
         />
-         <input
-          placeholder="Enter Security Name"
+        <input
+          placeholder="Security/Asset Id"
           value={securityName}
           onChange={(e) => setSecurityName(e.target.value)}
         />
+        {/* <input
+          placeholder="Transaction Type"
+          value={securityName}
+          onChange={(e) => setSecurityName(e.target.value)}
+        /> */}
         <Button
           variant="contained"
           onClick={() => SearchOrder()}
@@ -96,53 +92,56 @@ function PortfolioPage() {
         </Button>
       </Box>
 
-      {/* Simple Table */}
-      <Paper
-        elevation={3}
-        sx={{
-          width: "100%",
-          p: 1,
-          border: "1px solid #ddd",
-          overflowX: "auto",
-        }}
-      >
-        <table style={{ width: "100%", borderCollapse: "collapse" }}>
-          <thead>
-            <tr style={{ backgroundColor: "#1976d2", color: "white" }}>
-              <th>ID</th>
-              <th>Asset ID</th>
-              <th>Order Ref No</th>
-              <th>Transaction Type</th>
-              <th>Order Status</th>
-              <th>Order Value</th>
-              <th>Created On</th>
-              <th>Created By</th>
-            </tr>
-          </thead>
-          <tbody>
-            {historyData && historyData.length > 0 ? (
-              historyData.map((row) => (
-                <tr key={row.id}>
-                  <td>{row.id}</td>
-                  <td>{row.asset_id}</td>
-                  <td>{row.order_ref_no}</td>
-                  <td>{row.txn_type}</td>
-                  <td>{row.order_status ? "Active" : "Inactive"}</td>
-                  <td>{row.order_value}</td>
-                  <td>{row.created_on}</td>
-                  <td>{row.created_by}</td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan={8} style={{ textAlign: "center" }}>
-                  No data available
-                </td>
+        {/* Simple Table */}
+        <Paper
+          elevation={3}
+          sx={{
+            width: "100%",
+            p: 1,
+            border: "1px solid #ddd",
+            overflowX: "auto",
+          }}
+        >
+          <table style={{ width: "100%", borderCollapse: "collapse" }}>
+            <thead>
+              <tr style={{ backgroundColor: "#1976d2", color: "white" }}>
+                <th>ID</th>
+                <th>Asset ID</th>
+                <th>Asset Name</th>
+                <th>Order Ref No</th>
+                <th>Transaction Type</th>
+                <th>Order Status</th>
+                <th>Order Value</th>
+                <th>Created On</th>
+                <th>Created By</th>
               </tr>
-            )}
-          </tbody>
-        </table>
-      </Paper>
+            </thead>
+            <tbody>
+              {historyData && historyData.length > 0 ? (
+                historyData.map((row) => (
+                  <tr key={row.id}>
+                    <td>{row.id}</td>
+                    <td>{row.asset_id}</td>
+                    <td>{row.asset_name}</td>
+                    <td>{row.order_ref_no}</td>
+                    <td>{row.txn_type}</td>
+                    <td>{row.order_status ? "Active" : "Inactive"}</td>
+                    <td>{row.order_value}</td>
+                    <td>{row.created_on}</td>
+                    <td>{row.created_by}</td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={8} style={{ textAlign: "center" }}>
+                    No data available
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </Paper></>}
+
     </Box>
   );
 }
